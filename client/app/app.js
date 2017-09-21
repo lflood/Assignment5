d69f654c4914f3cc757c8fc494c123152199d9b4
Leena <lflood@ufl.edu>
2017-09-21 11:57:55 -0400

    with folders

---
+++ client/app/app.js
@@ -0,0 +1,25 @@
+/* register the modules the application depends upon here*/
+angular.module('listings', []);
+
+/* register the application and inject all the necessary dependencies */
+var app = angular.module('directoryApp', ['ui.router', 'ui.bootstrap', 'uiGmapgoogle-maps', 'listings']);
+
+/* application configuration */
+app.config(['$urlRouterProvider', '$locationProvider',
+  function($urlRouterProvider, $locationProvider) {
+    /* https://docs.angularjs.org/api/ng/provider/$locationProvider */
+    $locationProvider.html5Mode(true);
+
+    /* go to the '/listings' URL if an invalid route is provided */
+    $urlRouterProvider.otherwise('/listings');
+  }
+]);
+
+/* set the initial state of the application */
+app.run(['$state', 
+  function($state) {
+    $state.go('listings.list');
+  }
+]);
+
+/*should I add: 'ui.bootstrap.showErrors' inside brackets of first line*/
